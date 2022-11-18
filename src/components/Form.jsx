dc4eac5eabab3912b33ebd843319d528d16b422c
Facuscar <facundo.scardillo@dracoder.com>
2022-11-17 23:57:58 -0300

    Add: basic form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,26 @@
 const Form = () => {
     return (
-        <form>Form!</form>
+        <div className="container">
+            <form action="">
+                <div className="field">
+                    <label htmlFor="city">City</label>
+                    <input type="text" id="city" name="city" />
+                </div>
+                <div className="field">
+                    <label htmlFor="country">Country</label>
+                    <select name="country" id="country">
+                        <option value="">--Select a country--</option>
+                        <option value="US">United States of America</option>
+                        <option value="AR">Argentina</option>
+                        <option value="CL">Chile</option>
+                        <option value="FR">France</option>
+                        <option value="IT">Italy</option>
+                        <option value="ES">Spain</option>
+                    </select>
+                </div>
+                <input type="submit" value="Get weather" />
+            </form>
+        </div>
     );
 }
 
